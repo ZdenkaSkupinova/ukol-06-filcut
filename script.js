1b9300fb9a178bdce75ffecc3f8272786ab8d561
unknown <zdenka.x@post.czgitconfig user email zdenka.x@post.cz>
2023-11-27 23:24:52 +0100

    update

--- script.js
+++ script.js
@@ -5,37 +5,6 @@ Pokud je vstupní řetězec delší než len, tak funkce odřízne jeho konec a 
 Pokud je vstupní řetězec kratší než len, tak jej doplní od začátku znakem tečky a vrátí výsledek.
 Pokud je vstupní řetězec dlouhý přesně len, funkce jej vrátí beze změny.
 
-padStart(targetLength, padString) : Prodlouží řetězec na zadanou délku tak, že na začátek přidá opakování řetězce padString. Hodí se na zarovnávání nebo na formátování čísel.
- (("str".length + 1) === Number(len)) {
-    return str.padStart(0, " ")
-    } 
-document.body.innerHTML = `
-<p>${fillcut("honza", 2)}<p/>
-<p>${fillcut("kopretina", 4)}<p/>
-<p>${fillcut("zub", 10)}<p/>
-<p>${fillcut("petr", 9)}<p/>
-<p>${fillcut("oko", 3)}<p/>
-<p>${fillcut("kara", 4)}<p/>
-<p>${fillcut("honza", 2)}<p/>
-<p>${fillcut("zub", 10)}<p/>
-<p>${fillcut("petr", 9)}<p/>
-<p>${fillcut("oko", 3)}<p/>
-<p>${fillcut("kara", 4)}<p/>
-`
-
-
-document.body.innerHTML = `
-<p>${fillcut("honza", 2)}<p/>
-<p>${fillcut("kopretina", 4)}<p/>
-<p>${fillcut("zub", 10)}<p/>
-<p>${fillcut("petr", 9)}<p/>
-<p>${fillcut("oko", 3)}<p/>
-<p>${fillcut("kara", 4)}<p/>
-`
-
-
-Ještě jsem plichtila :D Výplňořez :D (alias fillcut, dú volitelné na doma) ze 7. lekce, tady si nejsem jistá, délku spočítá od nuly, přidám, ať je slovo stejně dlouhé jako číslo. Ale na <p>${fillcut("oko", 3)}<p/> mi to nefunguje, to "str" musí být stejné slovo? Mrkni prosím, díky moc. Zd.
-
 Ahoj, Ještě jsem to poupravila, doufám, že to je lepší. Mrkni, když tak odevzdám. Dáky.
 */
 
