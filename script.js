4d028a8a3b108408786b9609b6ab13bcaee81499
unknown <zdenka.x@post.czgitconfig user email zdenka.x@post.cz>
2023-11-30 16:47:47 +0100

    update

--- script.js
+++ script.js
@@ -6,17 +6,25 @@ Pokud je vstupní řetězec kratší než len, tak jej doplní od začátku znak
 Pokud je vstupní řetězec dlouhý přesně len, funkce jej vrátí beze změny.
 
 Ahoj, Ještě jsem to poupravila, doufám, že to je lepší. Mrkni, když tak odevzdám. Dáky.
+
+
+Ahoj Zdeňko, ten úkol vypadá dobře, akorát je tam drobná chybka, kvůli které to nedělá, co bylo zamýšleno. Tím, že máš v "str".length  uvozovky okolo str , se pak neporovnává délka řetězce v proměnné str, ale délka řetězce “str” – vždy tam tedy bude 3. Projeví se to pak třeba na těchto vstupních datech:
+Výsledkem fillcut("a", 3)  je “a” místo “..a”
+fillcut("dlouhý řetězec", 5)  vrací “dlouhý řetezec” místo “dlouh”
+
+
+
 */
 
 const fillcut = (str, len) => {
 
-    if(("str".length + 1) > Number(len)) {
+    if((str.length + 1) > Number(len)) {
     return str.slice(0, (str.length - str.length + len))
 
-    }else if(("str".length + 1) < Number(len)) {   
+    }else if((str.length + 1) < Number(len)) {   
     return str.padStart(len, ".")
 
-    }else if (("str".length + 1) === Number(len)) {
+    }else if ((str.length + 1) === Number(len)) {
     return str 
     }   
 }
